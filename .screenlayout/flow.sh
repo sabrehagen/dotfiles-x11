efd235eda4149deef9cb9b93c0885ab79792e5eb
Jackson Delahunt <jackson@stemn.com>
2020-03-29 16:37:08 +1100

    displaylink

--- .screenlayout/flow.sh
+++ .screenlayout/flow.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output DP-1 --mode 2560x1080 --pos 328x0 --rotate normal --output eDP-1 --primary --mode 3200x1800 --pos 0x1080 --rotate normal --output DP-2 --off
+xrandr --output DVI-I-1-1 --mode 1920x1080 --pos 4480x392 --rotate normal --output DVI-I-2-2 --mode 1920x1080 --pos 0x392 --rotate normal --output DP-1 --primary --mode 2560x1080 --pos 1920x0 --rotate normal --output eDP-1 --off --output DP-2 --off
+
