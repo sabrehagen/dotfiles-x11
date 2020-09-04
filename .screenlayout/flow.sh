cfa6a197f02fa265e54a729ed68f28884db29a16
Jackson Delahunt <jackson@stemn.com>
2020-09-04 14:09:22 +1000

    new

--- .screenlayout/flow.sh
+++ .screenlayout/flow.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-1-1 --mode 1920x1080 --pos 4480x392 --rotate normal --output DVI-I-2-2 --mode 1920x1080 --pos 0x392 --rotate normal --output DP-1 --primary --mode 2560x1080 --pos 1920x0 --rotate normal --output eDP-1 --off --output DP-2 --off
-
+xrandr --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 320x1440 --rotate normal --output DP-2 --off
